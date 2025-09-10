12a8636b95ceb05112bead89f4034a3342fe7860
Evolut <lucas.tortelli@hotmail.com>
2025-09-10 13:22:50 -0300

    feat: use ISO 8601 format for timestamp

--- src/app/api/last-update/route.ts
+++ src/app/api/last-update/route.ts
@@ -3,13 +3,6 @@ export async function GET() {
   
   return Response.json({
     timestamp,
-    formatted: new Date(timestamp).toLocaleString('pt-BR', {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit'
-    })
+    formatted: new Date(timestamp).toISOString().replace('T', ' ').slice(0, 19)
   });
 }
